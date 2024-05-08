e667e6d8a45d404edef338a9c64d3bc55c3ee5ff
ridhwan <mohamedridhwan8@gmail.com>
2024-05-09 01:04:32 +0300

    Rm

--- models.py
+++ models.py
@@ -87,12 +87,12 @@ class ServiceOrder(db.Model, SerializerMixin):
     
 
 
-class OrderItem(db.Model, SerializerMixin):
+class ProductOrderItem(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
 
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_order_id = db.Column(db.Integer, db.ForeignKey('product_order.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
     # serialize_rules = ('-order.order_items', '-product.orders',)
@@ -106,7 +106,7 @@ class ServiceOrderItem(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
 
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_order_id = db.Column(db.Integer, db.ForeignKey('product_order.id'), nullable=False)
     service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
 
     def __repr__(self):
